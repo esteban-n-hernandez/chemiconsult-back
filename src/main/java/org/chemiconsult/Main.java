786380f29d3d5390e2f26428eece788e39a0da62
esteban-n-hernandez <117520283+esteban-n-hernandez@users.noreply.github.com>
2024-01-08 13:41:26 -0300

    Heroku Port

--- src/main/java/org/chemiconsult/Main.java
+++ src/main/java/org/chemiconsult/Main.java
@@ -3,12 +3,19 @@ package org.chemiconsult;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        String port = System.getenv("PORT");
+        if (port == null || port.isEmpty()) {
+            port = "8080"; // Usar un puerto por defecto si no se encuentra la variable de entorno PORT
+        }
+
+        SpringApplication app = new SpringApplication(Main.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", port));
+        app.run(args);
     }
-
 }
-
