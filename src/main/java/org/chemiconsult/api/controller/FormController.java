9212ef7d08a45bca07b8386b8b7983aa61cb61b7
esteban-n-hernandez <117520283+esteban-n-hernandez@users.noreply.github.com>
2024-01-08 13:24:29 -0300

    SwaggerUI

--- src/main/java/org/chemiconsult/api/controller/FormController.java
+++ src/main/java/org/chemiconsult/api/controller/FormController.java
@@ -1,9 +1,7 @@
 package org.chemiconsult.api.controller;
 
 import org.chemiconsult.api.controller.inter.IFormController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -11,28 +9,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class FormController implements IFormController {
 
     @Override
-    @GetMapping("/create-form")
+    @PostMapping("/create-form")
     public void createForm() {
         System.out.println("FORM CREATED");
     }
 
     @Override
+    @DeleteMapping("/delete-form")
     public void deleteForm() {
         System.out.println("FORM DELETED");
     }
 
     @Override
+    @PutMapping("/update-form")
     public void updateForm() {
         System.out.println("FORM UPDATED");
     }
 
     @Override
+    @GetMapping("/get-forms")
     public void getForms() {
-
+        System.out.println("LIST FORM OBTAINED");
     }
 
     @Override
+    @GetMapping("/get-form/{id}")
     public void getForm() {
-
+        System.out.println("FORM OBTAINED");
     }
 }
