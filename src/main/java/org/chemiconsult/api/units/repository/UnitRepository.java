b9cf3c57a99c00a02ee94725f3218c9689620e8c
esteban-n-hernandez <117520283+esteban-n-hernandez@users.noreply.github.com>
2024-01-13 16:37:47 -0300

    Delete src/main directory

--- src/main/java/org/chemiconsult/api/units/repository/UnitRepository.java
+++
@@ -1,7 +0,0 @@
-package org.chemiconsult.api.units.repository;
-
-import org.chemiconsult.api.units.de.UnitDE;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UnitRepository extends JpaRepository<UnitDE, Integer> {
-}
