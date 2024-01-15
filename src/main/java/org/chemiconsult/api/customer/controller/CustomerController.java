58323743371f866e8651c20d580befc9f39ad067
esteban-n-hernandez <117520283+esteban-n-hernandez@users.noreply.github.com>
2024-01-15 16:39:56 -0300

    Update CustomerController.java

--- src/main/java/org/chemiconsult/api/customer/controller/CustomerController.java
+++ src/main/java/org/chemiconsult/api/customer/controller/CustomerController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/customer")
 public class CustomerController implements ICustomerController {
