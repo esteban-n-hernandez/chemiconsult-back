e3f374e49aea210e03098f324b87391e90fda010
esteban-n-hernandez <117520283+esteban-n-hernandez@users.noreply.github.com>
2024-01-22 22:14:53 -0300

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,18 @@
-CREATE TABLE customer_de (
+CREATE TABLE CUSTOMER (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255),
-    document_type VARCHAR(255) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100),
+    document_type VARCHAR(8) NOT NULL,
     document_number INT NOT NULL,
     phone_number INT,
     UNIQUE KEY unique_document (document_type, document_number)
+);
+
+
+CREATE TABLE RESOLUTION (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    NOMBRE_RESOLUCION VARCHAR(50) NOT NULL,
+    PARAMETRO VARCHAR(50) NOT NULL,
+    UNIT VARCHAR(50) NOT NULL,
+    LIMITE VARCHAR(50) NOT NULL
 );
